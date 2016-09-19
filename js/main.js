61412a053f63eb4c1f0f448ac92c542a38828566
dwulff <dwulff@users.noreply.github.com>
2016-09-20 00:12:34 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var myDataRef = new Firebase('https://password-protect.firebaseio.com/');
+
+
+function testFirebaseSet() {
+    var usersRef = myDataRef.child("users");
+    
+    var username = 'tricorius';
+    var email = 'tricorius@knightsoftheoldcode.com';
+    
+    var userRef = usersRef.child(username);
+    
+    userRef.set({email: email});
+};
+
+testFirebaseSet();
