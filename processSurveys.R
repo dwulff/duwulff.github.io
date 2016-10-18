8af7f7f1750b76c29759465fb463cc9289eaa5bc
dwulff <dirk.wulff@gmail.com>
2016-10-18 22:56:50 +0200

    new R package

--- processSurveys.R
+++ processSurveys.R
@@ -7,7 +7,7 @@ goodchoices = read.table('_Goodchoices/GoodchoicesSurveys.txt',header=F,sep='\n'
 networks    = read.table('_Networks/NetworksSurveys.txt',header=F,sep='\n')[,1]
 
 tab = paste0(networks[3],' (Antworten)')
-tab = paste0(goodchoices[2],' (Antworten)')
+tab = paste0(goodchoices[3],' (Antworten)')
 
 
 gs = gs_title(tab)
