0b2abd049b91fd88f8f0481ec42f7b250a94e1e9
dwulff <dirk.wulff@gmail.com>
2016-11-28 13:39:46 +0100

    new survey

--- processSurveys.R
+++ processSurveys.R
@@ -6,8 +6,8 @@ setwd('~/Dropbox (2.0)/Work/Software/dwulff.github.io/')
 goodchoices = read.table('_Goodchoices/GoodchoicesSurveys.txt',header=F,sep='\n')[,1]
 networks    = read.table('_Networks/NetworksSurveys.txt',header=F,sep='\n')[,1]
 
-tab = paste0(networks[6],' (Antworten)')
-tab = paste0(goodchoices[6],' (Antworten)')
+tab = paste0(networks[7],' (Antworten)')
+#tab = paste0(goodchoices[6],' (Antworten)')
 
 
 gs = gs_title(tab)
